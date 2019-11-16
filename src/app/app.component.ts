4c62466693b387f9acdd95266c7f70de88c50f19
Ana <rpoana26@gmail.com>
2019-11-15 21:02:45 -0500

    Initial events list, ngFor (complex structure)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  items: Array<any> = [
+  events: Array<any> = [
     {time: '08:00', subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
     {time: '08:30', subject: 'Daily Standup Meeting (recurring)', location: 'Warsaw Spire Office'},
     {time: '09:00', subject: 'Call with HRs'},
